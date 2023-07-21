f23ac5c438451890500092df772150a7da0cda8e
Eric Dupont <eric.dupont@hotmail.de>
2023-07-21 09:51:42 +0200

    Initial Commit

---
+++ DirectoryCopy/DirectoryCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirectoryCopyApp.swift
+//  DirectoryCopy
+//
+//  Created by Eric Dupont on 21.07.23.
+//
+
+import SwiftUI
+
+@main
+struct DirectoryCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
